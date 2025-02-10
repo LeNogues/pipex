8884ba5345b1ceda1d9ac411ecdb27153a912f6d
LeNogues <sebastienlenogues@gmail.com>
2025-02-10 19:02:10 +0100

    les cmd n effacent plus le infile

--- pipex.c
+++ pipex.c
@@ -22,6 +22,7 @@ int	verif_infile(char *file)
 		write(1, "file not found\n", 15);
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
 
@@ -35,29 +36,31 @@ int	verif_outfile(char *file)
 		write(1, "file not found\n", 15);
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
 
-int	pipex(int argc, char **argv, char **envp)
+int pipex(int argc, char **argv, char **envp)
 {
-	int	pipe_fd[2];
-	int	result;
+    int pipe_fd[2];
+    int result;
 
-	if (pipe(pipe_fd) == -1)
-		return (-3);
-	if (verif_infile(argv[1]) != -1)
-	{
-		result = handle_first_cmd(argc, argv, envp, pipe_fd);
-		if (result != 0)
-			return (result);
-	}
-	if (verif_outfile(argv[1]) != -1)
-	{
-		result = handle_second_cmd(argc, argv, envp, pipe_fd);
-		if (result != 0)
-			return (result);
-	}
-	close(pipe_fd[0]);
-	wait(NULL);
-	return (1);
+    if (pipe(pipe_fd) == -1)
+        return (-3);
+    if (verif_infile(argv[1]) != -1)
+    {
+        result = handle_first_cmd(argc, argv, envp, pipe_fd);
+        if (result != 0)
+            return (result);
+    }
+    if (verif_outfile(argv[4]) != -1)  // Correction ici
+    {
+        result = handle_second_cmd(argc, argv, envp, pipe_fd);
+        if (result != 0)
+            return (result);
+    }
+    close(pipe_fd[0]);
+    wait(NULL);
+    return (1);
 }
+
